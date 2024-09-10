c15bb29f2a36fcf36a21c0db82eee6abe10bfc98
Gustavo Freitas Rosa <heygu95@gmail.com>
2024-09-10 08:40:28 +0100

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Gustavo Freitas on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

251fc296fdb6092e8e1669c0adb45bbfe2645b35
Gustavo Freitas Rosa <heygu95@gmail.com>
2024-09-25 19:11:07 +0100

    Inserting of app icon, Launch and Onboard screens.

---
+++ PokeAPI/Views/OnboardView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardView.swift
+//  PokeAPI
+//
+//  Created by Gustavo Freitas on 25/09/2024.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardView()
+}
